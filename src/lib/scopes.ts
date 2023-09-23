1323a1609fd2045f136ab0ead501921995a41e02
Jan Heinemeyer <janheinemeyer99@gmail.com>
2023-09-23 20:05:00 +0200

    limit scope further

--- src/lib/scopes.ts
+++ src/lib/scopes.ts
@@ -1,6 +1,6 @@
 import { type Provider } from "./config";
 
 export const scopes: Record<Provider, string> = {
-  github: "public_repo,user",
+  github: "public_repo,user:email",
   gitlab: "api",
 };
