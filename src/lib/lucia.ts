7c42a9ee30e76246c776acf48295ab062be36b5c
Jan Heinemeyer <janheinemeyer99@gmail.com>
2023-10-07 18:37:16 +0200

    turso: use local db in dev mode

--- src/lib/lucia.ts
+++ src/lib/lucia.ts
@@ -3,10 +3,19 @@ import { astro } from "lucia/middleware";
 import { libsql } from "@lucia-auth/adapter-sqlite";
 import { createClient } from "@libsql/client";
 
-const db = createClient({
-    url: import.meta.env.TURSO_URL,
-    authToken: import.meta.env.TURSO_TOKEN
-});
+if (import.meta.env.DEV) {
+    console.log(`Running in DEV mode using sqlite db ${import.meta.env.TURSO_URL_DEV}`);
+}
+
+const db = import.meta.env.PROD ?
+    createClient({
+        url: import.meta.env.TURSO_URL,
+        authToken: import.meta.env.TURSO_TOKEN
+    })
+    : createClient({
+        url: import.meta.env.TURSO_URL_DEV,
+    });
+
 
 export const auth = lucia({
     env: import.meta.env.DEV ? "DEV" : "PROD",
