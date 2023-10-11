18ff5fac1da0409b95695048ae6e6deb05f94a2c
Jan Heinemeyer <janheinemeyer99@gmail.com>
2023-10-11 17:50:19 +0200

    daisyui logs: false

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [
-        require("@tailwindcss/typography"),
-        require("daisyui")
-    ],
-    daisyui: {
-        themes: ["light", "business"],
-        darkTheme: "business"
-    }
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["light", "business"],
+    darkTheme: "business",
+    logs: false,
+  },
 };
