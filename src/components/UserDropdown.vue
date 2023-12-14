8afe110c47a79b1b465d718ba5c9df4a545d7e81
Jan Heinemeyer <janheinemeyer99@gmail.com>
2023-12-14 18:41:00 +0100

    UserDropdown shadcn-vue

---
+++ src/components/UserDropdown.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { LogOut } from "lucide-vue-next";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const props = defineProps({
+    user: {
+        type: String,
+        required: true,
+        readonly: true,
+    },
+});
+</script>
+
+<template>
+    <DropdownMenu>
+        <DropdownMenuTrigger
+            class="text-muted-foreground open:text-primary-foreground focus:text-primary-foreground"
+            >{{ props.user }}</DropdownMenuTrigger
+        >
+        <DropdownMenuContent>
+            <DropdownMenuItem>
+                <a href="/new">Neues Rezept</a>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <LogOut class="mr-2 h-4 w-4" />
+                <form method="post" action="/logout">
+                    <input
+                        class="cursor-pointer"
+                        type="submit"
+                        value="Ausloggen"
+                    />
+                </form>
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
+</template>
