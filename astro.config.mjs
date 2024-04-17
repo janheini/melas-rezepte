8002f0cc767e6ee79c1f4805791b61b56bff1203
Jan Heinemeyer <janheinemeyer99@gmail.com>
2024-04-17 17:52:19 +0200

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
+import netlify from "@astrojs/netlify";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
     integrations: [vue(), tailwind()],
-    adapter: vercel({ imageService: true }),
+    adapter: import.meta.env.NETLIFY
+        ? netlify({ imageCDN: true, edgeMiddleware: true })
+        : vercel({ imageService: true }),
 });
+
