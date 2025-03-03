bc53ed5e44daf03631a5eac18f642fd5490fdf82
Jan Heinemeyer <janheinemeyer99@gmail.com>
2025-03-03 14:35:51 +0100

    remove netlify

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # melas-rezepte
-
-[![Netlify Status](https://api.netlify.com/api/v1/badges/c155050c-fa31-4602-9c54-5fdd97808337/deploy-status)](https://app.netlify.com/sites/melas-rezepte/deploys)
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import netlify from "@astrojs/netlify";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
     integrations: [vue(), tailwind()],
-    adapter:
-        process.argv[process.argv.length - 1] === "netlify"
-            ? netlify({ imageCDN: true, edgeMiddleware: false })
-            : vercel({ imageService: true }),
+    adapter: vercel({ imageService: true }),
 });
