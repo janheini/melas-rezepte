331c14f761f7d2d94378f51e101c07f338e27c09
Jan Heinemeyer <janheinemeyer99@gmail.com>
2025-03-06 06:53:19 +0100

    remove tailwind.config.mjs

--- tailwind.config.mjs
+++
@@ -1,9 +0,0 @@
-import typography from "@tailwindcss/typography";
-import headlessui from "@headlessui/tailwindcss";
-
-/** @type {import('tailwindcss').Config} */
-export default {
-    darkMode: "media",
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    plugins: [typography, headlessui],
-};
