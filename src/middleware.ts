9512ae5ab5c8728090c398954449ce9c1e72796f
Jan Heinemeyer <janheinemeyer99@gmail.com>
2025-08-20 18:07:26 +0200

    add note about csrf protection in astro 5

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,8 @@ import { validateSessionToken } from "@lib/session.ts";
 
 export const onRequest = defineMiddleware(async (context, next) => {
     // Check origin for anything but GET
+    // TODO: I think this is not necessary in Astro 5:
+    // https://docs.astro.build/en/guides/upgrade-to/v5/#csrf-protection-is-now-set-by-default
     if (context.request.method !== "GET" && import.meta.env.PROD) {
         const originHeader = context.request.headers.get("Origin");
         if (!originHeader || originHeader !== import.meta.env.SITE) {
